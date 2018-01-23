938f1f93441720feb3489f411968bff4cf8be246
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-01-23 16:53:11 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h> 
+int main() 
+{
+   int a[30],i,n,min; 
+   printf("Enter no of elements");
+   scanf("%d", &n);  
+   printf("Enter the elements")
+   scanf("%d", &a[i]);
+   }
+   min=a[i]; 
+   for(i=0;i<n;i++) 
+   {
+      if(a[i]>min)
+      {
+         min= a[i];
+      }
+   } 
+   printf("min Element",max); 
+   return 0;
+  }
