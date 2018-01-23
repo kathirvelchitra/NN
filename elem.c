b7a3b045254a4a0a4a8a99927076d7d78799d7db
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-01-23 16:58:51 +0530

    Create elem.c

---
+++ elem.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,n,median,sum=o;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+median=sum/n;
+printf("THE MEDIAN VALUE OF THE ARRAY",median);
+}
